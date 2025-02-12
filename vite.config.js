f294a6929246dd15cb7e1760ae2fe35a83c90312
Michelle <jiahu0724417@gmail.com>
2025-02-12 20:58:31 +0800

    edit root

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/reactweek-classproject6/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/reactweek-classproject5/' : '/',
   plugins: [react()],
 })
